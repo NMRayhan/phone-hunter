4ef5a9d521d716d4c7e80936cca80b2ea96c7209
NMRayhan <nurmdrayhan97@gmail.com>
2022-03-02 12:44:00 +0600

    Sensor list Showing More details

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ const SearchCheck = (data) => {
 
 // By-default autoload this Mobile
 function autoLoad() {
-  const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+  const url = `https://openapi.programming-hero.com/api/phones?search=mi`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => SearchCheck(data));
@@ -47,14 +47,12 @@ const loadDataInCard = (mobiles) => {
     // console.log(mobile);
     const cardCol = document.createElement("div");
     cardCol.classList.add("col-lg-4", "col-md-4", "col-sm-12", "mt-5");
-    cardCol.innerHTML = `<div class="card">
+    cardCol.innerHTML = `<div class="card shadow p-2 bg-body rounded" style="background-color: #69696917; min-height : 570px">
             <img src="${mobile.image}" alt="s" class="card-img-top card-img">
                 <div class="card-body">
                     <h2 class="cart-title"> <span class="text-warning fw-bold">Name :</span> ${mobile.phone_name}</h2>
                     <h2 class="cart-text"> <span class="text-success fw-bold">Brand :</span> ${mobile.brand}</h2>
-                </div>
-                <div class="card-footer">
-                    <button type="button" class="btn btn-outline-primary" onclick="moreBtnHandler('${mobile.slug}')" data-bs-toggle="modal" data-bs-target="#exampleModal">More Details</button>
+                    <button type="button" class="btn btn-outline-secondary" onclick="moreBtnHandler('${mobile.slug}')" data-bs-toggle="modal" data-bs-target="#exampleModal">More Details</button>
                 </div>
             </div>`;
     cardContainer.appendChild(cardCol);
@@ -72,7 +70,7 @@ const moreBtnHandler = (id) => {
 
 // moreDetails Showing 
 const moreDetails = (Details) =>{
-    console.log(Details);
+    // console.log(Details);
     document.getElementById('exampleModalLabel').innerText = Details.name
     const modalContainer = document.getElementById('modal-body')
     modalContainer.textContent = ''
@@ -86,17 +84,34 @@ const moreDetails = (Details) =>{
     <h5><i class="fas fa-check text-primary"></i> Processor : <span>${Details.mainFeatures.chipSet}</span></h5>
     <h5><i class="fas fa-check text-primary"></i> Display : <span>${Details.mainFeatures.displaySize}</span></h5>
     <h5><i class="fas fa-check text-primary"></i> Memory : <span>${Details.mainFeatures.memory}</span></h5>
+    <h5><i class="fas fa-check text-primary"></i> Storage : <span>${Details.mainFeatures.storage}</span></h5>
     <hr>
+
+    ${Details.others ? `
     <h2 class="text-center text-capitalize" style="color:gray">Specification</h2>
-    <h5><i class="fas fa-check text-primary"></i> Bluetooth : <span>${Details.others.Bluetooth}</span></h5>
-    <h5><i class="fas fa-check text-primary"></i> GPS : <span>${Details.others.GPS}</span></h5>
-    <h5><i class="fas fa-check text-primary"></i> NFC : <span>${Details.others.NFC}</span></h5>
-    <h5><i class="fas fa-check text-primary"></i> Radio : <span>${Details.others.Radio}</span></h5>
-    <h5><i class="fas fa-check text-primary"></i> USB : <span>${Details.others.USB}</span></h5>
-    <h5><i class="fas fa-check text-primary"></i> WLAN : <span>${Details.others.WLAN}</span></h5>
+    <h5><i class="fas fa-check text-primary"></i> Bluetooth : <span>${Details.others?.Bluetooth}</span></h5>
+    <h5><i class="fas fa-check text-primary"></i> GPS : <span>${Details.others?.GPS}</span></h5>
+    <h5><i class="fas fa-check text-primary"></i> NFC : <span>${Details.others?.NFC}</span></h5>
+    <h5><i class="fas fa-check text-primary"></i> Radio : <span>${Details.others?.Radio}</span></h5>
+    <h5><i class="fas fa-check text-primary"></i> USB : <span>${Details.others?.USB}</span></h5>
+    <h5><i class="fas fa-check text-primary"></i> WLAN : <span>${Details.others?.WLAN}</span></h5>
     <hr>
+    ` : `
+    <h2 class="text-center text-capitalize" style="color:gray">Sensor List</h2>
+        ${document.createElement('div').innerHTML = sensorListCreating(Details.mainFeatures.sensors).innerHTML}
+    `}
+
     `
     modalContainer.appendChild(modalBody);
 }
 
-// icon style 
+const sensorListCreating = (data) =>{
+    const sensorUl = document.createElement('ul');
+    for (let i = 0; i < data.length; i++) {
+        const list = data[i];
+        const sensorLi = document.createElement('li');
+        sensorLi.innerText = list;
+        sensorUl.appendChild(sensorLi);
+    }
+    return sensorUl;
+}
